a2578ad68d3bf6220f2571c4d79fafa010d96533
k45oo <k45oo@outlook.com>
2017-08-16 14:37:44 +0800

    d

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'Michael Liao'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 8088,
+        'user': 'root',
+        'password': '145236',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
